ab5c36ff6855e80188d93b9fbedcbfbc451d635c
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-05-15 18:20:19 -0300

    Add transactions,notification & wallet services to business domain

---
+++ Backend/broko-app/businessdomain/wallet-service/src/test/java/com/broko/app/wallet_service/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broko.app.wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
