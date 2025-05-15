1b63947714f470f6888cedabdb81a5a778b455f0
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-05-14 23:04:36 -0300

    Add Eureka to Infraestructure Domain

---
+++ Backend/broko-app/infrestructuredomain/eureka/src/test/java/com/broko/app/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broko.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
