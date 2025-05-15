8a24e2eecd9745604042a8f04e4feab0b1f9c137
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-05-15 18:34:24 -0300

    Add user-service & auth-service to business domain

---
+++ Backend/broko-app/businessdomain/auth-service/src/test/java/com/broko/app/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broko.app.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
