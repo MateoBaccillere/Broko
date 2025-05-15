f6d9680cf13eb6c72362ed5949b35a18c24b92c7
DMateo <93503252+Teobac123@users.noreply.github.com>
2025-05-15 18:24:44 -0300

    Add dashboard-service, conversion-service & fraud service to business domain

---
+++ Backend/broko-app/businessdomain/conversion-service/src/test/java/com/broko/app/conversion_service/ConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broko.app.conversion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Backend/broko-app/businessdomain/fraud-service/src/test/java/com/broko/app/fraud_service/FraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broko.app.fraud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
