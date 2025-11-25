11ebf31f06828c4fbb55cdee2178818d94b1f745
mateo.baccillere19 <mateo.baccillere19@gmail.com>
2025-11-25 10:17:07 -0300

    Deleted Backend and start again

--- Backend/broko-app/businessdomain/transactions-service/src/main/java/com/broko/app/transactions_service/model/TransactionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.broko.app.transactions_service.model;
-
-public enum TransactionStatus {
-    PENDING,
-    COMPLETED,
-    FAILED
-}
--- Backend/broko-app/businessdomain/wallet-service/src/main/java/com/broko/app/wallet_service/model/CurrencyType.java
+++
@@ -1,5 +0,0 @@
-package com.broko.app.wallet_service.model;
-
-public enum CurrencyType {
-    ARS, USD, EUR, BTC
-}
