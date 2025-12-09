4c14bd21b3fa6d508e996a7f6e3fb0c418f7a562
Mateo Baccillere <mateobaccillere1@gmail.com>
2025-12-09 19:37:02 -0300

    Backend Structure

--- Backend/broko-app/businessdomain/dashboard-service/src/main/java/com/broko/app/dashboard_service/repository/UserMetricsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.broko.app.dashboard_service.repository;
-
-import com.broko.app.dashboard_service.model.UserMetrics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserMetricsRepository extends JpaRepository<UserMetrics, UUID> {
-}
--- Backend/broko-app/businessdomain/wallet-service/src/main/java/com/broko/app/wallet_service/exception/WalletNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.broko.app.wallet_service.exception;
-
-import java.util.UUID;
-
-public class WalletNotFoundException extends BusinessException{
-    public WalletNotFoundException(UUID walletId) {
-        super("WALLET_NOT_FOUND", "No se encontró la wallet con ID: " + walletId);
-    }
-}
