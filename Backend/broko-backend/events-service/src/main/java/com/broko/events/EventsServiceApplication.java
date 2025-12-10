de1bb7c934b98f9850bcf5e93cc80e6be33b6ea2
Mateo Baccillere <mateobaccillere1@gmail.com>
2025-12-10 12:52:37 -0300

    Centralize Configuration

---
+++ Backend/broko-backend/events-service/src/main/java/com/broko/events/EventsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.broko.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EventsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventsServiceApplication.class, args);
+    }
+}
